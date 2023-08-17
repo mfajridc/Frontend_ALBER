04ecc5a4cb7fe08199bc273744b5bc50367bcd50
mfajridc <mfajridc@gmail.com>
2023-08-18 00:49:41 +0700

    APK new

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
  */
 
 import {registerRootComponent} from 'expo';
+import { AppRegistry } from 'react-native';
 import App from './App';
 registerRootComponent(App);
+AppRegistry.registerComponent('projectpertama', () => App)
 
 // import {name as appName} from './app.json';
 
